bfdbb327cb00671bfbf6f01252e25de04eace8ec
ailinnakaganeku <66168554+ailinnakaganeku@users.noreply.github.com>
2020-08-19 11:04:38 -0300

    homeworkterminada.js

--- 02-JS-I/homework/homeworkterminada.js
+++ 02-JS-I/homework/homeworkterminada.js
@@ -17,7 +17,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 4.2;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -183,7 +183,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + "" + nombre + "!";
+  return "Hola " + nombre + "!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
